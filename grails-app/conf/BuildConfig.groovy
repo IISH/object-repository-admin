53756396d6bb012c1cbd97a6032210067a4d8c2b
Lucien van Wouw <lwo@iisg.nl>
2012-05-07 12:58:57 +0200

    Setup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,8 +3,6 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.offline.mode = false
-//grails.project.war.file = 'ROOT.war'
-
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
