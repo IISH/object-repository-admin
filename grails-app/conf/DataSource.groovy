94662e9e21bfbfe9ac72217f13696560c7851290
Lucien van Wouw <lwo@iisg.nl>
2012-09-09 14:06:38 +0200

    Set readPreference to SEONDARY for the dissemination nodes.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,10 +1,17 @@
+import com.mongodb.ReadPreference
+
 // We use MongoDB for both test, dev and production environment.
+def _readPreference = (System.getProperty("layout", "not") == 'disseminate') ? ReadPreference.SECONDARY : ReadPreference.PRIMARY
+println("_readPreference=" + _readPreference)
 
 environments {
     production {
         dataSource {
             databaseName = "sa"
             host = "localhost" // mongos
+            options {
+                            readPreference = _readPreference
+                        }
         }
     }
     development {
