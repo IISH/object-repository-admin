48e74f33d18d692e2a96ded22f2fcc2fbaa385f3
Lucien van Wouw <lwo@iisg.nl>
2013-05-16 21:43:31 +0200

    Prevent nullpointer, use ?

--- grails-app/controllers/org/objectrepository/orfiles/OrfileController.groovy
+++ grails-app/controllers/org/objectrepository/orfiles/OrfileController.groovy
@@ -156,7 +156,7 @@ class OrfileController extends NamingAuthorityInterceptor {
         instruction.label = params.label
         instruction.task = [name: OrUtil.camelCase(['Instruction', actionName])]
         instruction.task.taskKey()
-        instruction.resubmitPid = orfileInstance?.metaData.pid
+        instruction.resubmitPid = orfileInstance?.metaData?.pid
 
         workflowActiveService.first(instruction)
         if (instruction.save(flush: true)) {
