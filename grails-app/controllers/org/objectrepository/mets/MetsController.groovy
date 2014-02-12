b66be9001e5a59287198b7efab5908d417324fd2
Lucien van Wouw <lwo@iisg.nl>
2014-02-12 12:01:03 +0100

    Update MetsController.groovy

--- grails-app/controllers/org/objectrepository/mets/MetsController.groovy
+++ grails-app/controllers/org/objectrepository/mets/MetsController.groovy
@@ -1,8 +1,5 @@
 package org.objectrepository.mets
 
-import grails.converters.XML
-import groovy.xml.MarkupBuilder
-
 /**
  * Offer a mets document on the fly based on a PID value or label
  */
@@ -11,6 +8,13 @@ class MetsController {
     def metsService
 
     def index() {
+
+        if (request.method.toLowerCase() == 'head') {
+            if (metsService.countMetsFile(params.na, params.objid) == 0)
+                return render(status: 404)
+            else
+                return render(status: 200, characterEncoding: 'utf-8', contentType: 'text/xml')
+        }
 
         def xml = metsService.writeMetsFile(params.na, params.objid)
         if (xml) {
